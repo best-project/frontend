26cc0119baa6034c3d7beef60fccfbf8fe20d46b
Marcin Josiński <marcin.josinski@skygate.io>
2019-12-31 14:46:32 +0100

    start working on course page

--- src/pages/course/index.js
+++ src/pages/course/index.js
@@ -3,8 +3,10 @@ import styled from 'styled-components';
 import { CoursesWrapperComponent } from '../../components/CoursesWrapper';
 import { HeaderComponent } from '../../components/Header';
 import { ContentComponent } from '../../components/Content';
-import { FooterComponent } from '../../components/Footer';
 import { courseService } from '../../services';
+import { Card } from 'antd';
+import { apiBase } from '../../config/variables';
+import EditableTable from '../../components/Table';
 
 const CoursePage = props => {
     const [courseDetails, setCourseDetails] = useState([]);
@@ -16,8 +18,22 @@ const CoursePage = props => {
     return (
         <CoursesWrapperComponent>
             <HeaderComponent />
-            <ContentComponent>{console.log(courseDetails)}</ContentComponent>
-            {/* <FooterComponent /> */}
+            <ContentComponent>
+                <Card title={courseDetails[0] && courseDetails[0].name} extra={<a href="#">Edit</a>}>
+                    <img
+                        style={{ height: 180 }}
+                        alt="example"
+                        src={courseDetails[0] && apiBase + courseDetails[0].image}
+                    />
+                    <p>
+                        <b>Description:</b> {courseDetails[0] && courseDetails[0].description}
+                    </p>
+                    <p>
+                        <b>Type:</b> {courseDetails[0] && courseDetails[0].type}
+                    </p>
+                </Card>
+                <EditableTable />
+            </ContentComponent>
         </CoursesWrapperComponent>
     );
 };
